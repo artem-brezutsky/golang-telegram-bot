138805390bc77c16205ba4d61298a6683d3b75ea
artem-brezutsky <a.brezutsky@gmail.com>
2022-06-18 13:06:05 +0300

    Create fetcher and processor

--- clients/events/telegram/telegram.go
+++ clients/events/telegram/telegram.go
@@ -1,11 +1,14 @@
 package telegram
 
-import "go-telegram-bot/clients/telegram"
+import (
+	"go-telegram-bot/clients/telegram"
+	"go-telegram-bot/lib/storage"
+)
 
 type Processor struct {
-	tg     *telegram.Client
-	offset int
-	// storage
+	tg      *telegram.Client
+	offset  int
+	storage storage.Storage
 }
 
 func New(c *telegram.Client) {
